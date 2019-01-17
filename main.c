eb288c06e760190ce9b9035376dcfec1357425d3
刘谨华 <3104587339@qq.com>
2019-01-17 08:28:18 +0800

    moxie

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("minganzifuguanlixitong\n");
+    while(1){
+        printf("1---tianjiaminganzifu\n");
+        printf("2---shanchuminganzifu(zuihouyige)\n");
+        printf("3---chakansuoyouminganzifu\n");
+        printf("4---tihuanneirongzhongdeminganzifu\n");
+        printf("5---tuichu\n");
+
+        printf("qingxuanzexiangyinggongneng");
+    }
 
     return 0;
 
