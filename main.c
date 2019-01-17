e96514809577f32f066ee2d87c2777cb2e4b59dc
刘谨华 <3104587339@qq.com>
2019-01-17 08:57:22 +0800

    moxie

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+char arr[1024];
+int index = 0;
+int isMingan(char x){
+    int count = 0;
+    for(int i=0;i<index;i++){
+        if(x == arr[i]){
+            count++;
+        }
+    }
+    if(count ==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 int main(){
 
     printf("minganzifuguanlixitong\n");
@@ -10,7 +24,62 @@ int main(){
         printf("4---tihuanneirongzhongdeminganzifu\n");
         printf("5---tuichu\n");
 
-        printf("qingxuanzexiangyinggongneng");
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+        if(code == 1){
+            printf("qingshuruyaotianjiademinganzife\n");
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            arr[index] = m;
+            index++;
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if(code == 2){
+            printf("shanchuzuihouyigeminganzi9fe\n");
+            index--;
+            printf("shanchuchenggong,dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if(code == 3){
+            printf("suoyouminganzifuruxia\n");
+            for (int i = 0;  i < index;i++){
+                printf("di%dgeminganzifuwei%c\n", i + 1, arr[i]);
+        }
+        printf("shanchuchenggong,dianjihuichejixu\n");
+        char x;
+        scanf("%c",&x);
+        scanf("%c",&x);
+        }
+        if(code == 4){
+            printf("qingshuruyiduanyingwen:\n");
+            char nerirong[1024];
+            scanf("%S",nerirong);
+            for(int i = 0;1;i++){
+                if(nerirong[i] == '\0'){
+                    break;
+                }
+                if(isMingan(nerirong[i]) ==1){
+                    nerirong[i] = '*';
+                }
+                printf("%c",nerirong[i]);
+            }
+            printf("\n");
+            printf("tihuanwancheng,dianjihuicehjixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
+        }
+        if(code == 5){
+            printf("tuichuxitong\n");
+            break;
+        }
     }
 
     return 0;
